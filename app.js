ba1ea1247d9718021ee047546f9d34ba9120f4d7
Iryna Predborska <iryna.predborska@gmail.com>
2020-05-12 18:42:02 +0200

    create a get request to "/" route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,23 +9,59 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+//Creating a schema and a model for a new item
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 
 app.get("/", function(req, res) {
-  const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items});
+  //Finding all the documents in the items collection
+  Item.find(function (err, docs) {
+    if (docs.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items in todolistDB!");
+          res.redirect("/");
+        }
+      });
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: docs});
+    }
+  });
 });
 
 app.post("/", function(req, res){
-  const item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+
+  const itemNew = new Item ({
+    name: itemName
+  });
+  itemNew.save();
+
+  res.redirect("/");
 });
 
 app.get("/work", function(req,res){
